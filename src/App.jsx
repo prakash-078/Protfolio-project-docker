3f711e5750378ecc5a0ff90ce6edfc197acb0775
Prakash Kuamr Mallik <mallikprakashkuamr56@gmail.com>
2026-06-23 18:47:01 +0530

    checking

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <>
     <header className='navbar'>
-      <h2>Prakash Kumar Mallik Student Gandhi </h2>
+      <h2>Prakash Kumar Mallik Student Gandhi Engineering College </h2>
       <nav>
         <a href="#about">About</a>
         <a href="#projects">Projects</a>
